445219f35d6c056da30737494f3e0fc0e3f0426d
LiKai <410592634@qq.com>
2023-08-14 21:57:49 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "KralBasic",
-            targets: ["KralObjc", "KralCommon", "KralProtocols"]),
+            targets: ["KralObjc", "KralCommon", "KralProtocols", "KralSwiftUI", "KralUIKit"]),
     ],
     dependencies: [
         .package(url: "https://gitee.com/krallee/DeviceKit.git", from: "4.0.0")
