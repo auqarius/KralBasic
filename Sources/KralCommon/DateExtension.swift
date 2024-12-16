8cc45fae67c8d304be8b249702a33c7224299297
LiKai <410592634@qq.com>
2024-12-16 21:20:24 +0800

    Update DateExtension.swift

--- Sources/KralCommon/DateExtension.swift
+++ Sources/KralCommon/DateExtension.swift
@@ -714,7 +714,7 @@ extension Date {
     /// 这几个天、周、月的对比，都要从当天的最后一秒来对比
     /// 因为切换成 0 时区的时间后，因为时差问题可能导致两个时间本来是一天后来不是一天，这样计算下来的 天、周、月 的数量就不对了
     /// 从开始的日期到现在的第几天
-    var daysFromStart: Int {
+    public var daysFromStart: Int {
         get {
             let days = Int(timeIntervalSince1970 / (24*3600))
             return days
